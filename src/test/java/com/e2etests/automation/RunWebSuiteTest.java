9113cbf43f4d3b3527efcadff88bff2013f43f7c
samahaouichaoui <samehaouichaoui@yahoo.fr>
2022-08-05 12:18:54 +0200

    Second commit

--- src/test/java/com/e2etests/automation/RunWebSuiteTest.java
+++ src/test/java/com/e2etests/automation/RunWebSuiteTest.java
@@ -10,7 +10,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = {"src/spec/features"}, 
-		plugin = {"pretty", "html:target/cucumber-report.html" }, 
+		plugin = {"pretty", "html:target/cucumber-report.html","json:target/cucumber.json" }, 
 		tags = ("@publication"),
 		monochrome = true, 
 		snippets = CAMELCASE
